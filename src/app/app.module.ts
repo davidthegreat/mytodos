cb9c29cb58b17a67600788619eeb4c5dbd6788d2
David Ramirez <davidsramirez11@gmail.com>
2017-09-03 12:24:17 -0700

    move all the components to a components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
-import { TodosComponent }  from './todos.component';
+import {AppComponent} from './components/app/app.component';
+import {TodosComponent} from './components/todos/todos.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
